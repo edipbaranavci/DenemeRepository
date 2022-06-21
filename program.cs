0ee8985e042a122f3bfb4456af601f8f859ef690
EdipBaranAVCI <33373519+edipbaranavci@users.noreply.github.com>
2022-06-21 23:53:30 +0300

    ddd

--- program.cs
+++ program.cs
@@ -1,8 +1,6 @@
 class Program
 {
     public static void Main(String[] args){
-        Console.WriteLine("Edip");
-        Console.WriteLine("Edip");
-        Console.WriteLine("Edip");
+        Console.WriteLine("Hello, i'm .... ");
     }
 }
