e3f6c6dd2988cc451bb94c2fbe927ed923493f7a
EdipBaranAVCI <33373519+edipbaranavci@users.noreply.github.com>
2022-07-31 00:19:31 +0300

    Update flutter_all_colors.dart

--- lib/flutter_all_colors.dart
+++ lib/flutter_all_colors.dart
@@ -1,16 +1,15 @@
-  Color generateRandomColor() {
-    // Define all colors you want here
-    const predefinedColors = [
-      Colors.red,
-      Colors.green,
-      Colors.yellow,
-      Colors.purple,
-      Colors.orange,
-      Colors.brown,
-      Colors.black,
-      Colors.white,
-    ];
-    Random random = Random();
-    int sayi = random.nextInt(predefinedColors.length);
-    return predefinedColors[sayi];
-  }
+class ColorConstants {
+  final List<Color> allColor = [
+    Colors.yellow,
+    Colors.red,
+    Colors.green,
+    Colors.deepPurple,
+    Colors.deepOrange,
+    Colors.brown,
+    Colors.blue,
+    Colors.grey,
+    Colors.cyan,
+    Colors.pink,
+    Colors.teal,
+  ];
+}
